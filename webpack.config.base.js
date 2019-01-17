28576659432a26894cafd8b7ae3c14cb868661b7
haozhaojie <haozhaojie@gotokeep.com>
2019-01-17 18:44:26 +0800

    webpack 优化

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -7,7 +7,8 @@ let base = {
   },
   plugins: [
     new htmlWebpackPlugin({
-      template: "src/index.html"
+      template: "src/index.html",
+      title: "My React"
     })
   ],
   module: {
@@ -15,17 +16,17 @@ let base = {
       {
         test: /\.(png|jpg|svg|gif|jpeg)$/i,
         use: ["file-loader?name=[name]-[hash].[ext]"],
-        include: path.resolve(__dirname,"src")
+        include: path.resolve(__dirname, "src")
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ["file-loader?name=[name]-[hash].[ext]"],
-        include: path.resolve(__dirname,"src")
+        include: path.resolve(__dirname, "src")
       },
       {
         test: /\.(jsx|js)$/,
-        use: ["babel-loader"],
-        include: path.resolve(__dirname,"src")
+        use: ["babel-loader?cacheDirectory=true"],
+        include: path.resolve(__dirname, "src")
       }
     ]
   },
@@ -42,6 +43,6 @@ let base = {
       }
     }
   }
- 
+
 };
 module.exports = base;
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,32 +1,41 @@
 const merge = require('webpack-merge');
-const webpack = require("webpack");
 const cleanWebpackPlugin = require("clean-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const base = require("./webpack.config.base");
 
-module.exports = merge(base,{
-    mode:"production",
+module.exports = merge(base, {
+    mode: "production",
     output: {
         path: __dirname + "/build",
-        filename: "[name]-[chunkhash].js"
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-              fallback: "style-loader",
-              use: 'css-loader'
-            })
-          },
-        ]
+        filename: "[name]-[contenthash].js"
     },
     plugins: [
         new cleanWebpackPlugin("build/*"),
-        new webpack.HashedModuleIdsPlugin(),
-        new ExtractTextPlugin({
-            filename: "[name]-[hash].css",
-            allChunks: false
-        })
-    ]
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            },
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true
+            }),
+            new OptimizeCssAssetsPlugin({})
+        ],
+    },
 })
